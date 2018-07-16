b20de1bb3f82e9dd09931de3c0a04e7d6b14afcc
zcolin <57521534@qq.com>
2018-07-16 10:59:34 +0800

    #增加js通讯示例

--- src/pages/js-bridge/js-bridge.ts
+++ src/pages/js-bridge/js-bridge.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { JsBridgeUtil } from '../../providers/jsbridge/jsbridge.util';
 import { UiService } from '../../providers/ui.service';
-import { JsUUIDReply } from '../../providers/jsbridge/jsbridge-option';
 
 @IonicPage()
 @Component({
@@ -10,7 +9,7 @@ import { JsUUIDReply } from '../../providers/jsbridge/jsbridge-option';
     templateUrl: 'js-bridge.html',
 })
 export class JsBridgePage {
-    imgsrc: string[];
+    imgsrc: string[] = [];
     constructor(public navCtrl: NavController, public navParams: NavParams, public uiService: UiService) {
     }
 
@@ -62,9 +61,13 @@ export class JsBridgePage {
     share() {
         JsBridgeUtil.share('我是title', '我是content', 'http://www.baidu.com');
     }
+
     selectImage() {
-        JsBridgeUtil.selectImage(1, 720, (reply) => {
-            this.imgsrc = reply.images;
+        JsBridgeUtil.selectImage(5, 720, (reply) => {
+            if (reply.images && reply.images.length > 0) {
+                this.imgsrc.length = 0;
+                this.imgsrc.push(...reply.images);
+            }
         });
     }
 }
