83fb295150b8e87ee4f40c4428ede0c643ce7792
zcolin <57521534@qq.com>
2018-12-25 17:57:16 +0800

    #工具类整合

--- src/components/arcgis-map/arcgis-map.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ArcgisMapComponent } from './arcgis-map';
-@NgModule({
-    declarations: [ArcgisMapComponent],
-    imports: [],
-    exports: [ArcgisMapComponent]
-})
-export class ArcgisMapModule { }
